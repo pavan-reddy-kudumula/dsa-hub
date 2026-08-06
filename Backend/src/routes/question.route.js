033a76d4ffd59bc279b7e70f7fecd637846d1f77
mightypavan <pavanreddy339900@gmail.com>
2026-08-06 17:48:35 +0530

    add get, post, patch and delete routes to question examples table

--- Backend/src/routes/question.route.js
+++ Backend/src/routes/question.route.js
@@ -1,12 +1,19 @@
 import express from "express"
 import protectRoute from "../middleware/auth.middleware.js";
 import verifyAdmin from "../middleware/admin.middleware.js";
-import { getQuestionById, updateQuestion, deleteQuestion } from "../controllers/question.controller.js"
+import { getQuestionById, getQuestionExamples, createQuestionExample, updateQuestion, updateQuestionExample, deleteQuestion, deleteQuestionExample } from "../controllers/question.controller.js"
 
 const router = express.Router();
 
 router.get("/:id", getQuestionById);
+router.get("/:id/examples", getQuestionExamples);
+
+router.post("/:id/examples", protectRoute, verifyAdmin, createQuestionExample);
+
 router.patch("/:id", protectRoute, verifyAdmin, updateQuestion);
+router.patch("/:id/examples/:exampleId", protectRoute, verifyAdmin, updateQuestionExample)
+
 router.delete("/:id", protectRoute, verifyAdmin, deleteQuestion);
+router.delete("/:id/examples/:exampleId", protectRoute, verifyAdmin, deleteQuestionExample);
 
 export default router;
