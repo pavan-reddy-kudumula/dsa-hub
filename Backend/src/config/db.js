0131a39fd258d03c12bfb772d1d3b6386c991444
mightypavan <pavanreddy339900@gmail.com>
2026-08-27 12:31:30 +0530

    change database connection configuration to connection string

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -6,11 +6,7 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  connectionString: process.env.DATABASE_URL_POOLED,
 })
 
 export default pool;
